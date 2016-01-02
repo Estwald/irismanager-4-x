509bae5edbe8344d1321eed9e404fee34bf88a11
Estwald <hermes.ps2@gmail.com>
2016-01-02 14:45:23 +0100

    v2.96 added sm.sprx

--- include/modules.h
+++ include/modules.h
@@ -6,4 +6,8 @@ extern unsigned char sprx_iso[SIZE_SPRX_ISO];
 
 #define PLUGIN_ISO "%s/sprx_iso" // note: it is also defined as sprx_iso from main.c
 
+#define SIZE_SPRX_SM 7456
+extern unsigned char sprx_sm[SIZE_SPRX_SM];
+#define PLUGIN_SM "%s/sprx_sm" // note: it is also defined as sprx_iso from main.c
+
 #endif
